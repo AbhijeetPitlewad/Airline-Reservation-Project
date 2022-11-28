6fc46d43149e1b602f321306b051b99bc3463942
SamarthNagmode <samarthnagmode5@gmail.com>
2022-11-28 15:18:37 +0530

    payment-1

--- Airline_Reservation_Backend_Project/src/main/java/com/lti/AirlineBackend/entity/Payment.java
+++ Airline_Reservation_Backend_Project/src/main/java/com/lti/AirlineBackend/entity/Payment.java
@@ -4,12 +4,49 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "Payments")
 public class Payment {
 	
 	@Id
-	private int PaymentId;
-	private double Amount;
-	private Date date;
+	private int paymentId;
+	private double amount;
+	private Date paymentDate;
+	public Payment(int paymentId, double amount, Date paymentDate) {
+		super();
+		this.paymentId = paymentId;
+		this.amount = amount;
+		this.paymentDate = paymentDate;
+	}
+	public Payment() {
+		super();
+	}
+	public int getPaymentId() {
+		return paymentId;
+	}
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public Date getPaymentDate() {
+		return paymentDate;
+	}
+	public void setPaymentDate(Date paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+	@Override
+	public String toString() {
+		return "Payment [paymentId=" + paymentId + ", amount=" + amount + ", paymentDate=" + paymentDate + "]";
+	}
+	
+	
+	
+	
 }
